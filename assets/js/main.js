662f081d2f47d1a9a2e7e5e5b4cbd841b578354f
C2002G <claudiogabriel2002@gmail.com>
2025-05-30 16:25:11 -0300

    mds

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,10 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 //fetch por padrao é uma requisição GET
 fetch(url) //interface de uma prommise
     .then((response) => response.json())//se funcionar recebe a resposta
-    .then((jsonBody) => console.log(jsonBody)) //converte a resposta para json e exibe no console
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+        console.log(pokemonList) //exibe a lista de pokemons no console
+    })
     .catch((error) => console.error('Erro ao buscar os dados:', error)) //se der erro, exibe o erro
 
 
