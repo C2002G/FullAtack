fed775eefd98b866e404dfd5a657eb59e49293b6
C2002G <claudiogabriel2002@gmail.com>
2025-05-30 16:25:18 -0300

    mds2

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,34 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-//fetch por padrao é uma requisição GET
-fetch(url) //interface de uma prommise
-    .then((response) => response.json())//se funcionar recebe a resposta
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemonList) => {
-        console.log(pokemonList) //exibe a lista de pokemons no console
-    })
-    .catch((error) => console.error('Erro ao buscar os dados:', error)) //se der erro, exibe o erro
+function convertPokemonToLi(pokemon){
+    return `<li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
 
 
-// https://pokeapi.co/api/v2/pokemon?offset=0&limit=10
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+
+                    </ol>
+
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                </div>
+            </li>`
+}
+const pokemonList = document.getElementById('pokemonList')
+
+
+fetch(url)
+     .then((response) => response.json())
+     .then((jsonBody) => jsonBody.results)
+     .then((pokemons) => {
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+            
+        }
+     })
+     .catch((error) => console.error('Error fetching data:', error))
