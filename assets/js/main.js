628738f487e5da3c619c315c7c4cb0966e4b5a20
C2002G <claudiogabriel2002@gmail.com>
2025-05-30 14:21:56 -0300

    arrowFunciton

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,9 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 //fetch por padrao é uma requisição GET
 fetch(url) //interface de uma prommise
-    .then(function(response){ //se funcionar recebe a resposta
-        console.log(response);
-    })
-    .catch(function(error){
-        console.error('Erro ao buscar os dados:', error); //se der erro, exibe o erro
-    })
-    .finally(function(){
-        console.log('Requisição finalizada'); //indica que a requisição foi finalizada idependentemente do sucesso ou falha
-    })
+    .then((response) => response.json())//se funcionar recebe a resposta
+    .then((jsonBody) => console.log(jsonBody)) //converte a resposta para json e exibe no console
+    .catch((error) => console.error('Erro ao buscar os dados:', error)) //se der erro, exibe o erro
+
 
 // https://pokeapi.co/api/v2/pokemon?offset=0&limit=10
